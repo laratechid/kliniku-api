e9be791601adcef74cb80e20b4a1d3e233e7f6ba
sholehbaktiabadi <sholehbaktiabadi@gmail.com>
2024-10-29 23:56:20 +0700

    chnage clinic details response

--- src/modules/clinic/clinic.service.ts
+++ src/modules/clinic/clinic.service.ts
@@ -3,9 +3,9 @@ import { ClinicRepository } from "./clinic.repository";
 import { response, responsePaginate } from "../../helper/response";
 import { PaginationDto } from "../../dto/pagination.dto";
 
-export class ClinicService{
+export class ClinicService {
     private clinicRepository: ClinicRepository
-    constructor(clinicRepository: ClinicRepository){
+    constructor(clinicRepository: ClinicRepository) {
         this.clinicRepository = clinicRepository
     }
 
@@ -15,7 +15,13 @@ export class ClinicService{
         return data
     }
 
-    async getAll(res: Res, pagination: PaginationDto){
-        return await this.clinicRepository.getAll(pagination)
+    async getAll(res: Res, pagination: PaginationDto): Promise<[any[], number]> {
+        const [data, count] = await this.clinicRepository.getAll(pagination)
+        const polyclinics: string[] = []
+        data.forEach(data => data.polyclinics
+            .forEach(data => polyclinics.push(data.poly.name)))
+        const response = data.map(res =>
+            ({ ...res, polyclinics, distance: "2.4 km", openSchedule: "08:00 - 21:00" }))
+        return [response, count]
     }
 }
