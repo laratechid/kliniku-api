7e2067173eaa84732b3e31b77f27ac5301ddbd9b
sholehbaktiabadi <sholehbaktiabadi@gmail.com>
2024-11-14 17:53:27 +0700

    feat(queue): add auto mock missing queue

--- src/helper/sort.ts
+++ src/helper/sort.ts
@@ -5,3 +5,21 @@ export function sortClinicSchedules(schedules: { day: Day }[]) {
     const dayOrder = Object.values(Day);
     return lo.sortBy(schedules, (schedule) => dayOrder.indexOf(schedule.day));
 }
+
+export function getMissingSequence(sequences: number[]): number[] {
+    let min: number = 1
+    if (sequences.length == 1) {
+        min = Math.min(1)
+    } else min = Math.min(...sequences);
+
+    const max = Math.max(...sequences);
+
+    const missingSequence = [];
+    for (let i = min; i <= max; i++) {
+        if (!sequences.includes(i)) {
+            missingSequence.push(i);
+        }
+    }
+
+    return missingSequence
+}
--- src/modules/polyclinic/polyclinic.service.ts
+++ src/modules/polyclinic/polyclinic.service.ts
@@ -6,26 +6,34 @@ import { response } from "../../helper/response";
 import { emitEvent } from "../../service/socket.io";
 import { Res } from "../../types/fastify";
 import { PolyClinicRepository } from "./polyclinic.repository";
+import { getMissingSequence } from "../../helper/sort";
 
-export class PolyClinicService{
+export class PolyClinicService {
     private polyClinicRepo: PolyClinicRepository
-    constructor(polyClinicRepository: PolyClinicRepository){
+    constructor(polyClinicRepository: PolyClinicRepository) {
         this.polyClinicRepo = polyClinicRepository
     }
 
-    async getDetail(res: Res, id: number){
+    async getDetail(res: Res, id: number) {
         const data = await this.polyClinicRepo.getDetail(id)
-        if(!data) return response(res, "not found", 400)
-        const { queues } = data
-        const totalQueue = (queues.length + 1)
-        for (let index = totalQueue ; index < totalQueue + 15 ; index++) {
-            const queue = new Queue() 
-            queue.status = "EMPTY" as QueueStatus
-            queue.sequence = index
-            queues.push(queue)
+        if (!data) return response(res, "not found", 400)
+        let { queues } = data
+
+        if (queues.length > 0) {
+            const sequenceCollections: number[] = []
+            queues.forEach(res => sequenceCollections.push(res.sequence))
+            const missingSequence = getMissingSequence(sequenceCollections)
+            missingSequence.forEach(res => {
+                const queue = new Queue() 
+                queue.status = "EMPTY" as QueueStatus
+                queue.sequence = res
+                queues.push(queue)
+            })
+            queues = _.sortBy(queues, 'sequence');
         }
+
         const totalRegistrant = _.size(_.filter(queues, item => item.status !== "EMPTY" as QueueStatus));
-        const result = {...data, totalRegistrant}
+        const result = { ...data, queues, totalRegistrant }
         emitEvent(res, polyEvent(id), result)
         return result
     }
